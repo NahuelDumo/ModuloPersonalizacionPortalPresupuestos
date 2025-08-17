74b2ef611e0d63701c7908143b89d61ac13e7471
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2025-08-17 11:32:33 -0300

    Primera prueba vista portal presupuesto

---
+++ sale_portal_quote_pdf_preview/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ModuloPersonalizacionPresupuesto',
+    'summary': 'Show/print/download the last attached PDF of a Sale Order in the customer portal.',
+    'version': '16.0.1.0.0',
+    'category': 'Sales/Portal',
+    'author': 'Nahuel Dumo',
+    'license': 'LGPL-3',
+    'depends': ['sale', 'portal', 'website'],
+    'data': [
+        # QWeb templates to be added after confirming Odoo version
+        'views/portal_templates.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'sale_portal_quote_pdf_preview/static/src/js/quote_print.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
